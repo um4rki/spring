f33c73688db45ac178631449e832e446ce29c297
umar <qarshiyevumar@gmail.com>
2025-01-07 19:39:57 +0500

    xml

---
+++ src/main/java/uz/pdp/Main.java
@@ -0,0 +1,11 @@
+package uz.pdp;
+
+/**
+ * Created by: Umar
+ * DateTime: 1/7/2025 7:31 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
